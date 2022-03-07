5388469315c65444501c8e4e065abe2f802ab48f
Xnanga <jpeutherer1@hotmail.co.uk>
2022-03-07 22:10:03 +0000

    Minor export removed.

--- src/Components/Meals/Meal.js
+++ src/Components/Meals/Meal.js
@@ -1,4 +1,4 @@
-import { useState, useContext, useEffect } from "react";
+import { useContext } from "react";
 
 import styles from "./Meal.module.css";
 import MealsContext from "../../Context/meals-context";
